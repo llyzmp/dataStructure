bfa06613748568facc87bbd87a616fe4f91c9364
lly <1776321381@qq.com>
2022-12-13 10:26:59 +0800

    feat: 翻转整数第二种方法

--- 4.字符串/1.翻转整数.js
+++ 4.字符串/1.翻转整数.js
@@ -16,28 +16,50 @@
  */
 
 // 示例
+// 方法一
+// function fn1(x) {
+//   if (typeof x !== 'number') return
+//   if (x === 0) return 0
+//   let result = ''
+//   const MAX = 2147483647
+//   const MIN = -2147483648
+//   let strNumArr = x.toString().split('').reverse()
+//   if (x < 0) {
+//     const index = strNumArr.findIndex(ele => ele === '-')
+//     strNumArr.splice(index, 1)
+//     strNumArr = ['-', ...strNumArr]
+//   }
+//   strNumArr.forEach(ele => {
+//     result += ele
+//   })
+//   result = Number(result)
+//   if(result > MAX || result < MIN) return 0
+//   console.log(result)
+//   return result
+// }
 
-function fn(x) {
-  if (typeof x !== 'number') return
-  if (x === 0) return 0
-  let result = ''
+// fn1(123)
+// fn1(-123)
+// fn1(120)
+
+
+// 方法二
+
+function fn2(x) {
+  if(typeof x !== 'number') return
   const MAX = 2147483647
   const MIN = -2147483648
-  let strNumArr = x.toString().split('').reverse()
-  if (x < 0) {
-    const index = strNumArr.findIndex(ele => ele === '-')
-    strNumArr.splice(index, 1)
-    strNumArr = ['-', ...strNumArr]
+  const rest = x > 0 ? String(x).split('').reverse().join('') : String(x).split('').slice(1).reverse().join('')
+
+  const result = x > 0 ? parseInt(rest, 10) : 0 - parseInt(rest, 10)
+
+  if (result < MAX && result > MIN) {
+    return result
   }
-  strNumArr.forEach(ele => {
-    result += ele
-  })
-  result = Number(result)
-  if(result > MAX || result < MIN) return 0
-  console.log(result)
-  return result
+  return 0
+
 }
 
-fn(123)
-fn(-123)
-fn(120)
+console.log(fn2(123))
+console.log(fn2(-123))
+console.log(fn2(120))
