416f7553d10594efe86b6e1c3b466fca9978bb6d
lly <1776321381@qq.com>
2023-03-29 00:19:01 +0800

    feat: 栈的实现

--- 1.数据结构与算法/01-栈/01-栈的实现.js
+++ 1.数据结构与算法/01-栈/01-栈的实现.js
@@ -13,18 +13,30 @@ class Stack {
     this.count++
   }
   pop() {
+    if (this.isEmpty()) {
+      console.log('栈为空')
+      return
+    }
     const temp = this.data[this.count - 1]
     delete this.data[--this.count]
     return temp
   }
   top() {
-
+    if (this.isEmpty()) {
+      console.log('栈为空')
+      return
+    }
+    return this.data[this.count-1]
   }
   size() {
-
+    return this.count
   }
   clear() {
-
+    this.data = []
+    this.count = 0
+  }
+  isEmpty() {
+    return this.count === 0
   }
 }
 const stack = new Stack()
