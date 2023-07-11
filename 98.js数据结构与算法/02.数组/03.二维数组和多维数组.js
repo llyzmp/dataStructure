062fe8dc79dea2c113abf27afb72699461c21123
lly <1776321381@qq.com>
2023-07-11 17:18:10 +0800

    feat: 数组

---
+++ 98.js数据结构与算法/02.数组/03.二维数组和多维数组.js
@@ -0,0 +1,32 @@
+let averageTempDay1 = [72, 75, 79, 79, 81, 81];
+let averageTempDay2 = [81, 79, 75, 75, 73, 72];
+
+// 可以使用二维数组
+var averageTemp = [];
+averageTemp[0] = [72,75,79,79,81,81];
+averageTemp[1] = [81,79,75,75,73,72];
+//day 1
+averageTemp[0] = [];
+averageTemp[0][0] = 72;
+averageTemp[0][1] = 75;
+averageTemp[0][2] = 79;
+averageTemp[0][3] = 79;
+averageTemp[0][4] = 81;
+averageTemp[0][5] = 81;
+// //day 2
+averageTemp[1] = [];
+averageTemp[1][0] = 81;
+averageTemp[1][1] = 79;
+averageTemp[1][2] = 75;
+averageTemp[1][3] = 75;
+averageTemp[1][4] = 73;
+averageTemp[1][5] = 72;
+
+function printMatrix(myMatrix) {
+  for (var i = 0; i < myMatrix.length; i++) {
+    for (var j = 0; j < myMatrix[i].length; j++) {
+      console.log(myMatrix[i][j]);
+    }
+  }
+}
+printMatrix(averageTemp)
